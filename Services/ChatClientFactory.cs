aa26ac58c4b03a5571df861fe3de71b114d29f53
jais260695 <aj936563@gmail.com>
2026-06-26 22:53:02 +0530

    added chat client factory

---
+++ Services/ChatClientFactory.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.AI;
+using OllamaSharp;
+
+
+namespace AgentOllamaPOC.Services;
+
+
+public class ChatClientFactory
+{
+
+    public IChatClient Create()
+    {
+
+
+        var ollama =
+            new OllamaApiClient(
+
+                new Uri(
+                    "http://localhost:11434"
+                ),
+
+                "llama3.1"
+
+            );
+
+
+
+        return new ChatClientBuilder(ollama)
+
+            .UseFunctionInvocation()
+
+            .Build();
+
+    }
+
+}
