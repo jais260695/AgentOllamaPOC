ca20ca719ae4be0f5b10e6ea59d354271fc7fff8
jais260695 <aj936563@gmail.com>
2026-06-29 21:23:43 +0530

    added tools
    added tools

---
+++ Tools/McpToolAdapter.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Client;
+using System.Text.Json;
+
+namespace AgentOllamaPOC.Tools;
+
+public class McpToolAdapter
+{
+    private readonly McpClient _client;
+
+    private ILogger<McpToolAdapter> _logger;
+
+    public McpToolAdapter(McpClient client, ILogger<McpToolAdapter> logger)
+    {
+        _client = client;
+        _logger = logger;
+    }
+
+    public async Task<List<AITool>> CreateToolsAsync()
+    {
+        var tools = await _client.ListToolsAsync();
+
+        //foreach (var t in tools)
+        //{
+        //    _logger.LogInformation("MCP Tool: {Tool} \n Schema: {Schema}", t.Name, t.JsonSchema);
+        //}
+
+        //Console.WriteLine("===== MCP TOOLS =====");
+
+        //foreach (var tool in tools)
+        //{
+        //    Console.WriteLine(tool.Name);
+
+        //    Console.WriteLine(tool.Description);
+
+        //    Console.WriteLine(
+        //        tool.JsonSchema
+        //    );
+        //}
+
+        //Console.WriteLine("====================");
+
+        var aiTools = new List<AITool>();
+
+        foreach (var mcpTool in tools)
+        {
+
+            //_logger.LogInformation("Registering MCP Tool {Tool}", mcpTool.Name);
+            var toolName = mcpTool.Name;
+
+            var function = AIFunctionFactory.Create(
+                                async (JsonElement jsonArguments) => {
+
+                                    _logger.LogInformation($"TOOL EXECUTED: {toolName}");
+
+                                    var arguments = JsonSerializer.Deserialize<Dictionary<string, object?>>(jsonArguments.GetRawText())?? new();
+
+                                    _logger.LogInformation(JsonSerializer.Serialize(arguments));
+
+                                    var result = await _client.CallToolAsync(toolName, arguments);
+
+                                    return result.Content.FirstOrDefault()?.ToString() ?? string.Empty;
+                                },
+                                mcpTool.Name,
+                                $"""
+                                {mcpTool.Description}
+
+                                IMPORTANT:
+                                Use this exact schema:
+
+                                {mcpTool.JsonSchema}
+
+                                Do not change parameter names.
+                                """
+                           );
+
+            aiTools.Add(function);
+        }
+        return aiTools;
+
+    }
+
+}
---
+++ Tools/RagToolService.cs
@@ -0,0 +1,50 @@
+﻿using AgentOllamaPOC.Models;
+using AgentOllamaPOC.Rag;
+using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AgentOllamaPOC.Tools;
+
+public class RagToolService
+{
+
+    private readonly RagService _rag;
+    private readonly ILogger<RagToolService> _logger;
+
+    public RagToolService(RagService rag, ILogger<RagToolService> logger)
+    {
+        _rag = rag;
+        _logger = logger;
+    }
+
+    public async Task<string> SearchRepositoryKnowledgeAsync([Required] string query)
+    {
+
+        _logger.LogInformation("RAG TOOL EXECUTED");
+
+        _logger.LogInformation(query);
+
+        return await _rag.SearchAsync(query);
+
+        //return await BuildContext(result);
+
+    }
+
+    public async ValueTask<string> BuildContext(List<RagSearchResult> results)
+    {
+        var sb = new StringBuilder();
+
+        var r = results.OrderByDescending(r=>r.Score).FirstOrDefault();
+        
+            //sb.AppendLine($"FILE: {r.FileName}");
+            //sb.AppendLine($"SCORE: {r.Score:0.00}");
+            //sb.AppendLine($"TYPE: {r.Type}");
+
+            //sb.AppendLine(new string('-', 60));
+        
+        _logger.LogInformation(sb.ToString());
+        return await ValueTask.FromResult(sb.ToString());
+    }
+
+}
